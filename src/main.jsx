814aedd2d2cc33aff9f40b7eef07f00fbd7cc02a
Даниил <kik9373976@gmail.com>
2026-07-17 18:02:32 +0500

    first Commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { SettingsProvider } from './context/SettingsContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MyResume">
       <SettingsProvider><App /></SettingsProvider>
     </BrowserRouter>
   </StrictMode>,
